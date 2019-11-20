cffd70933e51849da10c4fb0beaba45eee7f05c0
Viktor Speranskyy <vencedor@ukr.net>
2019-11-20 18:06:30 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Address address = new Address("65000","Odessa","Ukraine","Gagarinske plato 5b","5b","30");
+        System.out.println(address);
+
+    }
+}
